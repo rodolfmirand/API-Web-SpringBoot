50990a059c469e091753c7a23b98b1227f3485ae
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-05-06 13:44:28 -0300

    updt-tratamento-de-exceções

--- api/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ api/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -1,25 +1,28 @@
 package br.com.criandoapi.projeto.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import javax.swing.text.StyledEditorKit.BoldAction;
-
-import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.criandoapi.projeto.model.Usuario;
 import br.com.criandoapi.projeto.service.UsuarioService;
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin("*")
@@ -31,19 +34,30 @@ public class UsuarioController {
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
     }
-    
+
     @GetMapping
-    public ResponseEntity<List<Usuario>> listarUsuarios () {
+    public ResponseEntity<List<Usuario>> listarUsuarios() {
         return ResponseEntity.status(200).body(usuarioService.listarUsuarios());
     }
 
     @PostMapping
-    public ResponseEntity<Usuario> criarUsuario (@RequestBody Usuario usuario) {
+    public ResponseEntity<Usuario> criarUsuario(@Valid @RequestBody Usuario usuario) {
         return ResponseEntity.status(201).body(usuarioService.criarUsuario(usuario));
     }
 
+    @PutMapping
+    public ResponseEntity<Usuario> editarUsuario(@Valid @RequestBody Usuario usuario) {
+        return ResponseEntity.status(200).body(usuarioService.editarUsuario(usuario));
+    }   
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletarUsuario(@PathVariable Integer id) {
+        usuarioService.excluirUsuario(id);
+        return ResponseEntity.status(204).build();
+    }
+
     @PostMapping("/login")
-    public ResponseEntity<Usuario> validarSenha(@RequestBody Usuario usuario) {
+    public ResponseEntity<Usuario> validarSenha(@Valid @RequestBody Usuario usuario) {
         Boolean valid = usuarioService.validarSenha(usuario);
         if (!valid) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
@@ -52,14 +66,17 @@ public class UsuarioController {
         return ResponseEntity.status(200).build();
     }
 
-    @PutMapping
-    public ResponseEntity<Usuario> editarUsuario(@RequestBody Usuario usuario) {
-        return ResponseEntity.status(200).body(usuarioService.editarUsuario(usuario));
-    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deletarUsuario(@PathVariable Integer id) {
-        usuarioService.excluirUsuario(id);
-        return ResponseEntity.status(204).build();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
     }
-} 
+}
--- api/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
+++ api/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
@@ -6,6 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 //lombok construtores, getters e settes
@@ -24,15 +27,21 @@ public class Usuario {
     private Integer id;
 
     /* configura tamanho e não poder ser nulo */
-    @Column(name = "nome", length = 200, nullable = true)
+    @NotBlank(message = "O nome é obrigatório!")
+    @Size(min = 3, message = "O nome precisa ter no mínimo 3 caracteres.")
+    @Column(name = "nome", length = 200, nullable = false)
     private String nome;
 
-    @Column(name = "email", length = 50, nullable = true)
+    @Email(message = "Insira um e-mail válido!")
+    @NotBlank(message = "O e-mail é obrigatório! ")
+    @Column(name = "email", length = 50, nullable = false)
     private String email;
 
-    @Column(name = "senha", columnDefinition = "TEXT", nullable = true)
+    @NotBlank(message = "A senha é obrigatório!")
+    @Column(name = "senha", columnDefinition = "TEXT", nullable = false)
     private String senha;
 
-    @Column(name = "telefone", length = 15, nullable = true)
+    @NotBlank(message = "O telefone é obrigatório!")
+    @Column(name = "telefone", length = 15, nullable = false)
     private String telefone;
 }
